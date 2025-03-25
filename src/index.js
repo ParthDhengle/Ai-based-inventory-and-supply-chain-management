9d1ba5655af40ffb0499896fa7171273f44e59a5
Parth Dhengle <parthdhengle12@gmail.com>
2025-03-26 03:46:29 +0530

    changes 5 (close enough)
    its storing data but when on signup page, things got fked up*. and again that "john deo" thing(not properly fatching).

--- src/index.js
+++ src/index.js
@@ -4,3 +4,6 @@ import App from "../App"
 export default function Home() {
     return <App />
 }
+export default function Home() {
+  return <h1>Welcome to the Inventory and Supply Chain Management App</h1>;
+}
